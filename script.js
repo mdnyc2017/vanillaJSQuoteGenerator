109e230dcfe066068b2e8091cd51339b4e6603fe
Michael Douglas <michaelseandouglas@gmail.com>
2020-11-11 08:00:01 -0700

    working quote generator component using free api, google fonts, font awesome icons, and more. todo: create server to handle quote fetching, find more interesting quote api, enhance design

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log('testing');
+//TODO: find rad philosophy API;
+// Consts for DOM elements
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const quoteIcon = document.getElementById('quote-icon');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+};
+
+function hideLoadingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuoteFromApi(){
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blank, assign name Unknown
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font-size for long quotes:
+        if(data.quoteText.length > 50){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteIcon.classList.add('fas fa-quote-left');
+        quoteText.innerText = data.quoteText;
+
+        // Stop Loader, show quote:
+        hideLoadingSpinner();
+    } catch(err){
+        console.error(err)
+        // getQuote();
+    }
+};
+
+// Tweet Quote:
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+};
+
+// Event Listeners:
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+
+
+
+// On Load:
+getQuoteFromApi()
