9eae5f49b3c6ebb63870eab21b236873f3350bfb
Michael Douglas <michaelseandouglas@gmail.com>
2020-11-19 10:31:11 -0700

    functioning picture-in-picture working! using web video apis for the first time

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass it to video element, then play:
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(err){
+        console.error('ah jeeze, error: ', err)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button on click
+    button.disabled = true;
+
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+
+    // reset Button state
+    button.disabled = false;
+});
+
+
+// On Load:
+selectMediaStream();
